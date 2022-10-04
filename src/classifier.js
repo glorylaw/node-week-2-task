f111fec2135b300e872548329611c16705f3d81d
glorylaw <100371169+glorylaw@users.noreply.github.com>
2022-10-04 23:02:11 +0100

    week 2 task

--- src/classifier.js
+++ src/classifier.js
@@ -1,3 +1,88 @@
-function classifier(input) {}
+function classifier(input) {
+    //making a copy of the array so as not to mutate the original one
+    let arrayObj = input.slice()
+    const result = {
+        noOfGroups: 0
+    }
+    let id = 1
+
+    //invoking the ageDta function and sorting the age in ascending order
+    ageData(arrayObj)
+    const sortedArrayAge = arrayObj.sort((a,b) => a.age - b.age )
+    console.log(sortedArrayAge)
+
+    //using a for loop to dynamically create a group with its unique id
+    for(let i = 0; i < sortedArrayAge.length; i++){
+        //set the initial structure
+        result[`group${id}`] =  new Object()
+        result[`group${id}`].members = new Array()
+        result[`group${id}`].oldest = sortedArrayAge[i].age
+        result[`group${id}`].sum = sortedArrayAge[i].age
+        result[`group${id}`].regNos = new Array()
+
+        //populate the result with the first value
+        let noOfPushedItems = 1
+        let currentAge = sortedArrayAge[i].age
+        let ageDiff = null 
+        result[`group${id}`].members.push(sortedArrayAge[i])
+        result[`group${id}`].regNos.push(Number(sortedArrayAge[i].regNo))
+       
+       //the inner for loop continues from the next element while checking fr the conditions
+       //the maximum number of people in a group is 3
+       //the age difference between the group members should not be more than 5
+       //no 2 candidate should be in more than 1 group
+       for(let j = i + 1; j < sortedArrayAge.length; j++ ){
+        if (noOfPushedItems < 3){
+            
+            ageDiff = sortedArrayAge[j].age - currentAge
+            if(ageDiff <= 5){
+                result[`group${id}`].members.push(sortedArrayAge[j])
+                result[`group${id}`].sum += sortedArrayAge[j].age
+                result[`group${id}`].regNos.push(Number(sortedArrayAge[j].regNo))
+
+                if(sortedArrayAge[j].age > result[`group${id}`].oldest){
+                    result[`group${id}`].oldest = sortedArrayAge[j].age
+                }
+                //for every member that passes the condition it is removed and replaced by the next untill the loop ends
+                //while making sure the loops starts from the replace member.
+                let memberIndex = sortedArrayAge.indexOf(sortedArrayAge[j])
+                sortedArrayAge.splice(memberIndex, 1)
+                noOfPushedItems += 1
+                j = i
+            }
+
+        }
+       } 
+       //sorting the regNo in ascending order
+       result[`group${id}`].regNos.sort((a,b) => a - b)
+       result["noOfGroups"] += 1
+       id += 1 
+
+
+    }
+
+    return result
+}
+
+//the function below loops through the array of object,gets the age 
+// subtracts it from 2019, updates  and returns it
+const ageData = (arr) => {
+    arr.forEach((current) => {
+        let age = 2019 - new Date(current.dob).getFullYear()
+        current.age = age
+        return age;
+    })
+}
+
+
+
+// const dataNew = (array) =>{
+//     for(let i = 0; i < array.length; i++){
+//         let age = 2019 - new Date(array[i].dob).getFullYear() 
+//         array[i].age = age
+
+//     }
+//     return age;
+// }
 
 export default classifier;
--- src/duplicate-transactions.js
+++ src/duplicate-transactions.js
@@ -1,3 +1,87 @@
-function findDuplicateTransactions(transactions) {}
+function findDuplicateTransactions(transactions) {
+    //make a copy of the array and ensure to not mutate the original  array using slice method.
+    let result = []
+    let temporaryArray = []
+    const dataCopy = transactions.slice()
+    dataCopy.sort((a,b) => a.id - b.id)
+
+    //loop through the incoming transaction according to their sourceAccount`, `targetAccount`, `category`, `amount` and time
+    for(let i = 0; i < dataCopy.length; i++ ){
+        const sourceAccount = dataCopy[i].sourceAccount
+        const targetAccount = dataCopy[i].targetAccount
+        const category = dataCopy[i].category
+        const amount = dataCopy[i].amount
+        const transactionDay = new Date(dataCopy[i].time).getDate()
+        const transactionMonth = new Date(dataCopy[i].time).getMonth() + 1
+        const transactionYear = new Date(dataCopy[i].time).getFullYear() 
+
+        // push the item into the temporary array
+        temporaryArray.push(dataCopy[i])
+
+        //create another for loop that starts from the next element and validates similar transaction.
+        //if a similar transaction is found,it is pushed to the temp array, removed and replaced by the next transaction.
+        for(let j = i + 1; j < dataCopy.length; j++){
+            if(dataCopy[j].sourceAccount === sourceAccount &&
+                dataCopy[j].targetAccount === targetAccount &&
+                dataCopy[j].category === category &&
+                dataCopy[j].amount === amount &&
+                new Date(dataCopy[j].time).getDate() === transactionDay &&
+                new Date(dataCopy[j].time).getMonth() + 1 === transactionMonth &&
+                new Date(dataCopy[j].time).getFullYear() === transactionYear                
+            ){
+                temporaryArray.push(dataCopy[j])
+                let index = dataCopy.indexOf(dataCopy[j])
+                dataCopy.splice(index, 1)
+                j = i
+            }
+
+        }
+         //temporary array which contains similar transactions is then pushedinto the result array
+         result.push(temporaryArray)
+         //empty the temporary array.
+         temporaryArray = []
+    }
+
+    //every group with a single transaction is removed, this is achieved by looping through the result array
+    for (let i = 0; i < result.length; i++){
+        if(result[i].length === 1) {
+            let index = result.indexOf(result[i])
+            result.splice(index, 1)
+            i = index - 1 
+        }
+    }
+
+    //for every group having similar details,difference of time is checked.
+    //if the transaction is less than 1 mintute,it is tagged as a duplicate
+    for(let i = 0; i < result.length; i++){
+        let firstTransac = result[i][i].time
+        for(let j = 0; j < result[i].length; j++){
+            let secondTransac = result[i][j].time
+            let valid = compareTime(firstTransac, secondTransac)
+            if(valid){
+                firstTransac = secondTransac
+            }else{
+                let index = result[i].indexOf(result[i][j])
+                result[i].splice(index, 1)
+            }
+        }
+    }
+    
+    return result
+
+}
+// create a function that accepts two arguments(objects),gets their time and compares if the difference is less than a minute
+const compareTime = (timeA, timeB) => {
+    let seconds = 0
+    let time = new Date(timeB).getMinutes() - new Date(timeA).getMinutes()
+
+    if(time > 1){
+        return false
+    }else if( time === 1){
+        seconds = new Date(timeB).getSeconds() - new Date(timeA).getSeconds()
+        return seconds < 0
+    }
+    return true
+}
 
 export default findDuplicateTransactions;
